steamwings/fizzy

commit 250935530f86eb628d6b54ec6b7212dac9e76f8b

Better focus and clearing when searching

app/javascript/controllers/bar_controller.js
@@ -1,5 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 import { post } from "@rails/request.js"
+import { nextFrame } from "helpers/timing_helpers";
 
 export default class extends Controller {
   static targets = [ "turboFrame", "search", "searchInput", "form", "buttonsContainer" ]
@@ -19,6 +20,11 @@ export default class extends Controller {
 
     this.#showItem(this.buttonsContainerTarget)
     this.#hideItem(this.searchTarget)
+  }
+
+  clearInput() {
+    this.searchInputTarget.value = ""
+    this.searchInputTarget.focus()
   }
 
   showModalAndSubmit(event) {
@@ -50,10 +56,12 @@ export default class extends Controller {
     this.turboFrameTarget.innerHtml = ""
   }
 
-  #showItem(element) {
+  async #showItem(element) {
     element.removeAttribute("hidden")
 
     const autofocusElement = element.querySelector("[autofocus]")
+
+    await nextFrame()
     autofocusElement?.focus()
     autofocusElement?.select()
   }

app/assets/stylesheets/card-columns.css
@@ -176,6 +176,10 @@
     [aria-selected] & .card[aria-selected] {
       outline: var(--focus-ring-size) solid var(--color-selected-dark);
       outline-offset: var(--focus-ring-offset);
+
+      @media (prefers-color-scheme: dark) {
+        outline-color: oklch(var(--lch-blue-medium));
+      }
     }
   }
 

app/views/searches/_form.html.erb
@@ -9,7 +9,7 @@
         data: {
           bar_target: "searchInput",
           action: "keydown.enter->bar#showModalAndSubmit:prevent keydown.esc->bar#reset" } %>
-  <button class="search__reset btn btn--circle borderless" data-action="bar#reset">
+  <button class="search__reset btn btn--circle borderless" data-action="bar#clearInput">
     <%= icon_tag "close" %>
     <span class="for-screen-reader">Close search</span>
   </button>

commit 0c05694a3c001c96bdee0ec8c8f4d92f3b302b5c

This seems to have caused the unwanted effect of not focusing the field in Mobile Safari
Looks like it regressed in
https://github.com/basecamp/fizzy/commit/250935530f86eb628d6b54ec6b7212dac9e76f8b.
Let's see if we can get focus in Safari without sacrificing select in
other browsers by simply moving the nextFrame() call

app/javascript/controllers/bar_controller.js
@@ -65,8 +65,8 @@ export default class extends Controller {
 
     const autofocusElement = element.querySelector("[autofocus]")
 
-    await nextFrame()
     autofocusElement?.focus()
+    await nextFrame()
     autofocusElement?.select()
   }
 